7aac44cf52d1cdba9cab1d2b6cf0fd6a49f32740
hinapupil <hinapupil@gmail.com>
2020-07-19 19:33:48 +0900

    from -> form

--- src/index.js
+++ src/index.js
@@ -13,32 +13,65 @@ try {
 }
 
 var count = 0;
-var text = "";
+var sql_result = "";
 
 //create a server object:
 http
   .createServer(function (req, res) {
     let url = req["url"];
     switch (true) {
-      case /db$/.test(url):
+      case /^\/top.*/.test(url):
+        console.log(url);
         res.writeHead(200, {
           "Content-Type": "text/html; charset=utf-8"
         });
         res.write("<html><body><table border>");
-
+        let query = url.split("?");
+        let parameters = {};
+        if (query.length >= 2) {
+          for (let param of query[1].split("&")) {
+            let key, value;
+            [key, value] = param.split("=");
+            parameters[key] = value;
+          }
+          console.log(parameters);
+        }
+        let direction;
+        if( parameters[ "desc" ] ) direction = "";
+        else direction = " desc ";
+        let limit = " limit " + parameters[ "number" ];
         db.serialize(() => {
-          db.each("select id, 都道府県, cast(学生数 as real) * 100 / cast(人口 as real) as result from example order by result desc limit 10;", (error, row) => {
+          db.each("select id, 都道府県, 人口 from example order by 人口" + direction + limit + ";", (error, row) => {
             if (error) {
               console.log('Error: ', error);
               return;
             }
-            text += "<tr><td>" + row.id + "</td><td>" + row.都道府県 + "</td><td>" + row.result + "</td></tr>";
+            sql_result += "<tr><td>" + row.id + "</td><td>" + row.都道府県 + "</td><td>" + row.人口 + "</td></tr>";
           });
         });
-        res.write(text);
+        res.write(sql_result);
+        res.write("</table></body></html>");
+        break;
+      case /db$/.test(url):
+        console.log(url);
+        res.writeHead(200, {
+          "Content-Type": "text/html; charset=utf-8"
+        });
+        res.write("<html><body><table border>");
+        db.serialize(() => {
+          db.each("SELECT id, 都道府県, 人口 FROM example order by id;", (error, row) => {
+            if (error) {
+              console.log('Error: ', error);
+              return;
+            }
+            sql_result += "<tr><td>" + row.id + "</td><td>" + row.都道府県 + "</td><td>" + row.人口 + "</td></tr>";
+          });
+        });
+        res.write(sql_result);
         res.write("</table></body></html>");
         break;
       case /counter$/.test(url):
+        console.log(url);
         count++;
         res.writeHead(200, {
           "Content-Type": "text/html; charset=utf-8"
@@ -61,6 +94,7 @@ http
           res.write(error);
         }
     }
+    sql_result = ""; //refresh
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
